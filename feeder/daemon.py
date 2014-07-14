b57512b6d3086de7174297e0f9ba53d38590afae
Jon de Andres <jon@wuaki.tv>
2014-07-14 02:32:08 +0200

    Add ZMQ receiver.

--- feeder/daemon.py
+++ feeder/daemon.py
@@ -2,8 +2,8 @@
 
 import sys, os, base64
 import time, random
-import threading
 from  feeder import yowsup_patch
+from feeder.zmq import receiver
 from feeder.zmq import sender
 
 from Yowsup.connectionmanager import YowsupConnectionManager
@@ -13,36 +13,30 @@ class Daemon:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-        self.zmq_sender = sender.Sender()
-
-        yowsup_patch.patch(self.zmq_sender)
-
         self.connectionManager = YowsupConnectionManager()
         self.methodsInterface = self.connectionManager.getMethodsInterface()
         self.signalsInterface = self.connectionManager.getSignalsInterface()
+        self.zmq_sender = sender.Sender()
+        self.zmq_receiver = receiver.Receiver(self.methodsInterface)
 
         self.setup()
 
-
     def setup(self):
+        yowsup_patch.patch(self.zmq_sender)
         YowsupDebugger.enabled=False
+
         self.connectionManager.setAutoPong(True)
         self.signalsInterface.registerListener("auth_success", self.onAuthSuccess)
         self.signalsInterface.registerListener("auth_fail", self.onAuthFailed)
         self.signalsInterface.registerListener("disconnected", self.onDisconnected)
         self.signalsInterface.registerListener("receipt_messageDelivered", self.onMessageDelivered)
+        self.signalsInterface.registerListener("message_received", self.onMessageReceived)
+        self.signalsInterface.registerListener("group_messageReceived", self.onGroupMessageReceived)
 
     def run(self):
         self.login()
-        thread = threading.Thread(target=self.sender_function)
-        thread.start()
-        thread.join
-
-    def sender_function(self):
-        while True:
-            time.sleep(random.randint(5, 15))
-            self.sendMessage('foo')
+        self.zmq_receiver.bind()
+        self.zmq_sender.connect()
 
     def login(self):
         password = base64.b64decode(bytes(self.password.encode('utf-8')))
@@ -52,6 +46,16 @@ class Daemon:
     def onMessageDelivered(self, jid, messageId):
         print "Message was delivered successfully to %s" %jid
         self.methodsInterface.call("delivered_ack", (jid, messageId))
+
+    def messageACK(self, jid, messageId):
+        print 'Sending ACK'
+        self.methodsInterface.call('message_ack', (jid, messageId))
+
+    def onMessageReceived(self, messageId, jid, messageContent, timestamp, wantsReceipt, pushName, isBroadcast):
+        self.messageACK(jid, messageId)
+
+    def onGroupMessageReceived(self, messageId, jid, msgauthor, messageContent, timestamp, wantsReceipt, pushName):
+        self.messageACK(jid, messageId)
 
     def onAuthSuccess(self, username):
         print "Authed %s" % username
--- feeder/zmq/sender.py
+++ feeder/zmq/sender.py
@@ -4,12 +4,9 @@ class Sender:
     def __init__(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
+
+    def connect(self):
         self.socket.connect('tcp://127.0.0.1:5555')
-        self.send('Connected!')
-
-
-    def deliver(self, node):
-        print(node)
 
     def send(self, msg):
         self.socket.send(msg)

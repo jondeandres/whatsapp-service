26b07a5beba96eab7b2014268896d221363b70ac
igayoso <igayoso@gmail.com>
2014-08-07 20:13:13 +0200

    Added other simyo number for tests

--- bin/run.py
+++ bin/run.py
@@ -7,5 +7,4 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 from feeder import daemon
 
 daemon.run('34684070575', 'xDkWCwXBOVcCLWpOM5I0oI1nu7w=')
-
-
+#daemon.run('34644298488', 'iggEzRpQKOA16GvCCPDF6n6qX4A=')
